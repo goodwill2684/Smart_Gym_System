1d4c1954211c62e1e7764bdb592d4a1b7d218ac6
limsan <dlatksaaa@naver.com>
2021-10-20 19:07:44 +0900

    1020 1907

---
+++ app/src/main/java/com/jicode/smartgymsystem/adapter/DeviceAdapter.java
@@ -0,0 +1,34 @@
+package com.jicode.smartgymsystem.adapter;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.jicode.pm_monitor.R;
+import com.vise.baseble.model.BluetoothLeDevice;
+import com.vise.xsnow.ui.adapter.helper.HelperAdapter;
+import com.vise.xsnow.ui.adapter.helper.HelperViewHolder;
+
+
+public class DeviceAdapter extends HelperAdapter<BluetoothLeDevice> {
+
+    public DeviceAdapter(Context context) {
+        super(context, R.layout.item_scan_layout);
+    }
+
+    @Override
+    public void HelpConvert(HelperViewHolder viewHolder, int position, BluetoothLeDevice bluetoothLeDevice) {
+        TextView deviceNameTv = viewHolder.getView(R.id.device_name);
+        TextView deviceMacTv = viewHolder.getView(R.id.device_mac);
+        TextView deviceRssiTv = viewHolder.getView(R.id.device_rssi);
+        if (bluetoothLeDevice != null && bluetoothLeDevice.getDevice() != null) {
+            String deviceName = bluetoothLeDevice.getDevice().getName();
+            if (deviceName != null && !deviceName.isEmpty()) {
+                deviceNameTv.setText(deviceName);
+            } else {
+                deviceNameTv.setText("Unknown device");
+            }
+            deviceMacTv.setText(bluetoothLeDevice.getDevice().getAddress());
+            deviceRssiTv.setText("RSSI:" + bluetoothLeDevice.getRssi() + "dB");
+        }
+    }
+}
